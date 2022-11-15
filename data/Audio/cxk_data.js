b3dbf4e50ca3718c322c2449922104b6d339cee6
BoYs <75466116+BoYs-J@users.noreply.github.com>
2022-11-15 16:00:59 +0800

    修改js

--- data/Audio/cxk_data.js
+++ data/Audio/cxk_data.js
@@ -1,17 +1,18 @@
-const cxk_Audio = [
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_grlxs.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_hber.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_hcydsj.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_jntm.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_lblhnkg.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_ngmay.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_nmzgm.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_nwsmyszzh.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_okok.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_qmzzr.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_wsh.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_wsmnzmh.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_wzdbx.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/cxk_ylyl.mp3",
-    "https://cdzero.gitee.io/zero/data/Audio/xck_ggjyg.mp3"
-]
+function cxk_Audio(){
+    let Audio = ["https://cdzero.gitee.io/zero/data/Audio/cxk_grlxs.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/cxk_hber.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/cxk_hcydsj.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/cxk_jntm.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/cxk_lblhnkg.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/cxk_ngmay.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/cxk_nmzgm.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/cxk_nwsmyszzh.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/cxk_okok.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/cxk_qmzzr.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/cxk_wsh.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/cxk_wsmnzmh.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/cxk_wzdbx.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/cxk_ylyl.mp3",
+                     "https://cdzero.gitee.io/zero/data/Audio/xck_ggjyg.mp3"]
+    return Audio
+};
